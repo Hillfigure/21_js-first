3039256c2ba62581403eb84a0a0bb5cbc4f6f095
Hillfigure <49096115+Hillfigure@users.noreply.github.com>
2021-10-16 16:24:50 +0200

    Add files via upload

---
+++ 21_first-js/script.js
@@ -0,0 +1,19 @@
+document.write("<h3>Document gebruikt ipv loggen, hopelijk is dat ook goed :)</h3>")
+document.write("Hello Winc Academy");
+document.write("</br>");
+let awesomeName = "Robert";
+document.write(awesomeName);
+document.write("</br>");
+let result = 4 + 4;
+document.write("4 + 4 = " + result);
+document.write("</br>");
+document.write("No, you cannot add strings without casting")
+document.write("</br>");
+let intOne = parseInt("4");
+let intTwo = parseInt("6");
+document.write("Use parseInt: 4 + 6 = " + (intOne + intTwo));
+document.write("</br>");
+document.write("</br>");
+document.write("intOne is now typeOf: " + typeof(intOne));
+document.write("</br>");
+document.write("intOne is now typeOf: " + typeof(intOne.toString()));
